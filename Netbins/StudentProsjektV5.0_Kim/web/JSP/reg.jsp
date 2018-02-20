e04e827f5fddec9f6be2f334be6863a0d7f4aa71
KimMoe <by-crom@hotmail.com>
2018-02-20 13:17:40 +0100

    k
    k

---
+++ Netbins/StudentProsjektV5.0_Kim/web/JSP/reg.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Registrer ny bruker</title>
+</head>
+<body>
+<%@ page import ="java.sql.*" %>
+<%@ page import ="javax.sql.*" %>
+<%@ page import ="Java.Tools" %>
+<%
+ /**
+String user=request.getParameter("userid"); 
+session.putValue("userid",user); 
+* **/
+
+Class.forName("com.mysql.jdbc.Driver"); 
+            String first;
+            String last;
+            String pass;
+            String email;
+            String status;
+            first = request.getParameter("fname");
+            last = request.getParameter("lname");
+            pass = request.getParameter("pwd");
+            email = request.getParameter("email");
+            status = "Student";
+            Tools dbTools = new Tools();
+            dbTools.connect();
+            dbTools.newUser(first, last, pass, email, status);
+
+
+out.println("Registered"); 
+
+
+%>
+<a href ="Login.html">Login</a><br/><br/>
+<a href="index.html">Home</a>
+</body>
+</html>
---
+++ Netbins/StudentProsjektV5.0_KimV2/src/java/Java/Tools.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java;
+
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Dette er en Java klasse som er laget for å holde metoder som bruker av flere
+ * sider. Dette har vi valgt å gjøre for å ikke ha duplisering av kode, samt å
+ * lette kunne lese koden.
+ *
+ * @author by-cr
+ */
+public class Tools {
+
+    String URL = "jdbc:mysql://localhost:3306/slit";
+    String USERNAME = "root";
+    String PASSWORD = "root";
+    Connection connection = null;
+    PreparedStatement insertUsers = null;
+    PreparedStatement selectUsers = null;
+    PreparedStatement selectUsersDel = null;
+    PreparedStatement deleteUser = null;
+    PreparedStatement feedback = null;
+    ResultSet resultSet = null;
+
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            Logger.getLogger(Tools.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    //Henter ut en liste med studenter og modulpoeng.
+    public void User() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            selectUsers = connection.prepareStatement(
+                    /*  "SELECT user_fname, user_lname, user_email, m1_points, m2_points, m3_points, m4_points, m5_points FROM user, m1, m2, m3, m4, m5 where user.user_id  = m1.user_id and user.user_id = m2.user_id and user.user_id = m3.user_id and user.user_id = m4.user_id and user.user_id = m5.user_id;"); */
+                    //" SELECT user_fname, user_lname, user_email, user_m1, user_m2, user_m3, user_m4, user_m5 FROM user where user_status='Student' ORDER BY FIELD(user_fname, '"+user+"') DESC");
+
+                    " SELECT user_fname, user_lname, user_email, user_m1, user_m2, user_m3, user_m4, user_m5 FROM user;");
+
+        } catch (SQLException e) {
+        }
+    }
+
+    public ResultSet getUser() {
+        try {
+            resultSet = selectUsers.executeQuery();
+
+        } catch (SQLException e) {
+        }
+
+        return resultSet;
+    }
+
+    //Lager en ny bruker i databasen
+    public void newUser(String first, String last, String password, String email, String status) {
+
+        PreparedStatement newStud;
+        int result = 0;
+        try {
+            insertUsers = connection.prepareStatement(
+                    "INSERT INTO user (user_fname, user_lname, user_password, user_email, user_status)"
+                    + " VALUES (?, ?, ?, ?, ?)");
+            insertUsers.setString(1, first);
+            insertUsers.setString(2, last);
+            insertUsers.setString(3, password);
+            insertUsers.setString(4, email);
+            insertUsers.setString(5, status);
+            result = insertUsers.executeUpdate();
+
+        } // end try     
+        catch (SQLException ex) {
+            ex.printStackTrace();
+
+        }
+    }
+
+    // Sletter en bruker fra databasen. Gir en alfabetisk liste hvor du hvelger hvem som slettes. Slettes basert på id.
+    public void Delete() {
+        try {
+
+            selectUsersDel = connection.prepareStatement(
+                    "SELECT user_id, user_fname, user_lname FROM user ORDER BY user_fname");
+
+            deleteUser = connection.prepareStatement(
+                    "DELETE FROM user WHERE user_id = ?");
+        } catch (SQLException e) {
+        }
+
+    }
+
+    public ResultSet getUserDel() {
+
+        try {
+            resultSet = selectUsersDel.executeQuery();
+        } catch (SQLException e) {
+        }
+
+        return resultSet;
+    }
+
+    public int deleteUser(Integer id) {
+        int result = 0;
+
+        try {
+            deleteUser.setInt(1, id);
+            result = deleteUser.executeUpdate();
+        } catch (SQLException e) {
+        }
+        return result;
+    }
+
+    public void createFeedback(String fId, String content, int points, int sId) {
+
+        PreparedStatement newStud;
+        int result = 0;
+        try {
+            feedback = connection.prepareStatement(
+                    "INSERT INTO feedback (feedback_id, feedback_content, feedback_points, submit_id)"
+                    + " VALUES (?, ?, ?, ?)");
+            feedback.setString(1, fId);
+            feedback.setString(2, content);
+            feedback.setInt(3, points);
+            feedback.setInt(4, sId);
+            result = feedback.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        }
+    }
+    
+    public void setModluler(int id, int m1, int m2, int m3, int m4 ,int m5) throws SQLException
+    {
+        Connection con = null;
+    PreparedStatement pst = null;
+    ResultSet rs =  null;
+
+      try{
+            String sql = "UPDATE user SET  user_m1=?,user_m2=?,user_m3=?,user_m4=?,user_m5=? WHERE user_id=?";
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/slit","root","root");
+            pst = con.prepareStatement(sql);
+
+            
+            pst.setInt(1,m1);
+            pst.setInt(2,m2);
+            pst.setInt(3,m3);
+            pst.setInt(4, m4);
+            pst.setInt(5,m5);
+            pst.setInt(6,id);
+
+            pst.executeUpdate();
+
+        }  
+      catch (SQLException ex) {
+            Logger.getLogger(Tools.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+    }
+
+    public void sok(String Navn) throws SQLException {
+        String navn = Navn;
+        java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/slit", "root", "root");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from user where user_fname='" + navn + "'");
+        if (rs.next()) {
+            if (rs.getString(2).equals(navn)) {
+                rs = st.executeQuery("SELECT user_fname, user_lname, user_email, user_m1, user_m2, user_m3, user_m4, user_m5 FROM user WHERE user_fname='" + navn + "'");
+            }
+
+        } else {
+            if (rs.getString(2).equals(navn)) {
+                rs = st.executeQuery("SELECT user_fname, user_lname, user_email, user_m1, user_m2, user_m3, user_m4, user_m5 FROM user WHERE user_fname='" + navn + "'");
+            }
+        }
+
+        out.println("Ingen ved navnet " + Navn + " funnet");
+
+    }
+}
